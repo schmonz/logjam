ac79020a1ec06ad92096e4c872a52af08e7567d9
Amitai Schlair <schmonz-web-ikiwiki@schmonz.com>
2013-01-15 10:02:45 -0500

    Make the tests pass.

--- logjam.pl
+++ logjam.pl
@@ -5,6 +5,33 @@ package logjam;
 use warnings;
 use strict;
 
+sub parse_log_line {
+	my ($line) = @_;
+	my $NUM_FIELDS = 6;
+
+	my @numbered_fields = split(/ /, $line, $NUM_FIELDS);
+
+	die "too few fields"
+		if $NUM_FIELDS > @numbered_fields;
+
+	my %named_fields = (
+		month	=> $numbered_fields[0],
+		day	=> $numbered_fields[1],
+		time	=> $numbered_fields[2],
+		host	=> $numbered_fields[3],
+		tag	=> $numbered_fields[4],
+		message	=> $numbered_fields[5],
+	);
+
+	die "not our application"
+		if 'OpaqueImportantThing:' ne $named_fields{tag};
+
+	die "no message logged"
+		if '' eq $named_fields{message};
+
+	return %named_fields;
+}
+
 sub main {
 	exit(0);
 }
